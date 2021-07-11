5490e27fa470eb07803831ecbdb4d8858ebf88be
NikolayIT <tkachenni@mail.ru>
2021-07-11 19:54:07 +0300

    First verson recognise

--- HandRecognosionSimple.py
+++ HandRecognosionSimple.py
@@ -6,19 +6,36 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
-mpDraw = 
+mpDraw = mp.solutions.drawing_utils
+
+cTime = 0
+pTime = 0
 
 while True:
-    success, img =cap.read()
+    success, img = cap.read()
 
     imgRGB = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    results = hands.proccess(imgRGB)
+    results = hands.process(imgRGB)
 
     print(results)
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                #print(cx, cy)
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255, cv2.FILLED))
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1 / (cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
     cv2.imshow("Picture", img)
     cv2.waitKey(1)
