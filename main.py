0f1011a5368082720c6a374278adc65331179c11
NikolayIT <tkachenni@mail.ru>
2021-07-11 20:14:34 +0300

    Complete Hands recognise

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from HandRecognisionHandsModule import app_start
 
 
 def print_hi(name):
@@ -12,5 +13,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    app_start()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
